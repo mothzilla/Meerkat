2b29fdbc84a64926f67d2ba000e504d095988bc9
mhall99 <erinmhall61@gmail.com>
2023-02-11 17:44:32 -0600

    User chooses size of class (up to 10 students), error handling for exceeding max amount

--- StudentDatabase/main.c
+++ StudentDatabase/main.c
@@ -5,16 +5,23 @@ struct student {
     char firstName[50];
     int roll;
     float marks;
-} s[5];
+} s[10];
 
 int main()
 {
     //printf("Hello world!\n");
     int i;
-    printf("Please enter the information of 5 students:\n");
+    int numberOfStudents;
+    printf("This database can hold up to 10 student.\nHow many students would you like to enter into the database?\n");
+    scanf("%d", &numberOfStudents);
+    if (numberOfStudents>10){
+        printf("Too many students! Try again when you can follow directions...");
+        exit(EXIT_FAILURE);
+    }
+    //printf("Please enter the information of 5 students:\n");
 
     //begin to store info
-    for (i=0;i<5;i++) {
+    for (i=0;i<numberOfStudents;i++) {
         s[i].roll = i+1;
         printf("For student with roll number %d, please enter first name:\n", s[i].roll);
         scanf("%s", s[i].firstName);
@@ -24,7 +31,7 @@ int main()
     printf("Displaying information about current roster...\n");
 
     //displaying...
-    for (i=0;i<5;i++){
+    for (i=0;i<numberOfStudents;i++){
         printf("Name: %s\nRoll: %d\nMarks: %.2f\n", s[i].firstName, s[i].roll, s[i].marks);
         printf("-----------------------------\n");
     }

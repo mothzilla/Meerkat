9901cec22da4231ec30b8f1e313fde4fd7c10273
mhall99 <erinmhall61@gmail.com>
2023-02-11 12:54:43 -0600

    Simple calculator

--- VariablesAndBasicOps/main.c
+++ VariablesAndBasicOps/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    char name[] = "Numcy";
+    float valueOne;
+    float valueTwo;
+    float answer;
+    char op;
+    printf("Welcome! My name is %s and I will be your friendly calculator today.\nPlease enter your operation:", name);
+    scanf("%f %c %f", &valueOne, &op, &valueTwo);
+    //printf("This -> \" <- is how we render special characters.");
+    switch(op)
+    {
+    case '/':
+        answer = valueOne/valueTwo;
+        break;
+    case '*':
+        answer = valueOne*valueTwo;
+        break;
+    case '+':
+        answer = valueOne+valueTwo;
+        break;
+    case '-':
+        answer = valueOne-valueTwo;
+        break;
+    case '^':
+        answer = pow(valueOne,valueTwo);
+        break;
+    default:
+        goto fail;
+    }
+    printf("%.9g %c %.9g = %.6g\n", valueOne, op, valueTwo, answer);
     return 0;
+
+    fail:
+        printf("Epic fail.\n");
+        exit(EXIT_FAILURE);
 }
